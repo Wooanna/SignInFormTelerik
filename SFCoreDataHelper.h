434f896956b556c0ee75895f47b38764e458bdff
Yoanna Mareva <jo.thirdeyevision@gmail.com>
2014-12-13 01:36:13 +0200

    Added core data helper

--- SFCoreDataHelper.h
+++ SFCoreDataHelper.h
@@ -7,7 +7,15 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import <CoreData/CoreData.h>
 @interface SFCoreDataHelper : NSObject
+
+@property(nonatomic,strong) NSManagedObjectContext* context;
+@property(nonatomic, strong) NSManagedObjectModel* model;
+@property(nonatomic, strong) NSPersistentStoreCoordinator* coordinator;
+@property(nonatomic, strong) NSPersistentStore* store;
+
+- (void)saveContext;
+- (void)setupCoreData;
 
 @end
